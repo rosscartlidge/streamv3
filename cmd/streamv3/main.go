a64c2df9b12cf1c9c34dd25ee75d1dc3a0aef9b3
Ross Cartlidge <rossc@google.com>
2025-11-01 12:13:39 +1100

    Add version to generated code comment
    - Generated code now shows 'Generated by streamv3 vX.Y.Z:'
    - Version constant added to lib package
    - Helps with reproducibility and debugging
    - Version bump to 0.10.3

--- cmd/streamv3/main.go
+++ cmd/streamv3/main.go
@@ -24,7 +24,7 @@ func main() {
 		printUsage()
 		os.Exit(0)
 	case "-version", "--version", "version":
-		fmt.Println("streamv3 version 0.10.2")
+		fmt.Println("streamv3 version 0.10.3")
 		os.Exit(0)
 	case "-bash-completion":
 		printBashCompletion()
